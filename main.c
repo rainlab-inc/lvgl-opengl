83d10adbef6c294c9db66c0d77a47a323f877b7d
Tan Aytekin <tanaytekin@riseup.net>
2022-02-17 10:13:28 +0300

    mouse driver

--- main.c
+++ main.c
@@ -16,6 +16,13 @@ static void hal_init(void)
     static lv_disp_drv_t disp_drv;
     sdl_gles_disp_drv_init(&disp_drv, &draw_buf);
     lv_disp_t *disp = lv_disp_drv_register(&disp_drv);
+
+    static lv_indev_drv_t indev_drv_1;
+    lv_indev_drv_init(&indev_drv_1);
+    indev_drv_1.type = LV_INDEV_TYPE_POINTER;
+    indev_drv_1.read_cb = sdl_gles_mouse_read;
+    lv_indev_t *mouse_indev = lv_indev_drv_register(&indev_drv_1);
+
 }
 
 
@@ -24,9 +31,9 @@ int main()
     lv_init();
     hal_init();
 
-    //lv_example_btn_1();
+    lv_example_btn_1();
     //lv_example_btn_2();
-    lv_example_checkbox_1();
+    //lv_example_checkbox_1();
 
     while (1) {
         lv_timer_handler();
